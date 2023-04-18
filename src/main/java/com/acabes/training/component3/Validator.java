c04e37abda4c6bfbe9dc2aa4896e9fec67abb16e
Omar Othman <omar-othman@acabesinternational.com>
2023-04-18 10:34:11 +0300

    Initial Commit

--- src/main/java/com/acabes/training/component3/Validator.java
+++ src/main/java/com/acabes/training/component3/Validator.java
@@ -1,5 +1,19 @@
 package com.acabes.training.component3;
 
 public class Validator {
+    public void setStartingFile(String path) {
 
+    }
+
+    public void setFullFile(String path) {
+
+    }
+
+    public void setDataBase() {
+
+    }
+
+    public void validate() {
+
+    }
 }
