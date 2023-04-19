c65a5c4cc376e1837d3c0e2b61d79e8ca229d457
THINPAD_12 <ragood.200024@gmail.com>
2023-04-19 11:49:24 +0300

    make each name has an id

--- src/main/java/com/acabes/training/component2/Mdm.java
+++ src/main/java/com/acabes/training/component2/Mdm.java
@@ -1,52 +1,61 @@
 package com.acabes.training.component2;
 
-import com.acabes.training.Utils;
-
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Mdm {
 
-	private final ArrayList<Transaction> initialTransactions ;
-	private final ArrayList<FullTransaction> fullTransactions ;
-	private final  ArrayList<String> names;
-	Random random = new Random();
+    private final ArrayList<Transaction> initialTransactions;
+    private final ArrayList<FullTransaction> fullTransactions;
+    private final ArrayList<String> names;
+    Random random = new Random();
+    private final HashMap<Integer, String> uniqueNames;
 
-	public Mdm(ArrayList<Transaction> initialTransactions, ArrayList<String> names) throws IOException {
+    public Mdm(ArrayList<Transaction> initialTransactions, ArrayList<String> names) throws IOException {
 
-		this.initialTransactions = initialTransactions;
-		this.names = names;
+        this.initialTransactions = initialTransactions;
+        this.names = names;
+        this.uniqueNames = new HashMap<>();
 
-		fullTransactions = new ArrayList<>();
-		getFullTransactionData();
-		new Writer(fullTransactions);
-	}
+        fullTransactions = new ArrayList<>();
+        getFullTransactionData();
+        new Writer(fullTransactions);
+    }
 
-	public void getFullTransactionData() {
-		for (Transaction transaction : initialTransactions) {
-			createFullTransaction(transaction);
-		}
+    public void getFullTransactionData() {
+        for (Transaction transaction : initialTransactions) {
+            createFullTransaction(transaction);
+        }
 
-	}
+    }
 
-	private void createFullTransaction(Transaction transaction) {
-		int fromAccountID = transaction.getFromAccountNumber();
-		int toAccountID = transaction.getToAccountNumber();
-		double amount = transaction.getAmount();
-		String fromAccountName = getAccountName();
-		String toAccountName = getAccountName();
-		fullTransactions.add(new FullTransaction(fromAccountID, toAccountID, amount, fromAccountName, toAccountName));
-	}
+    private void createFullTransaction(Transaction transaction) {
+        int fromAccountID = transaction.getFromAccountNumber();
+        int toAccountID = transaction.getToAccountNumber();
+        double amount = transaction.getAmount();
 
-	private String getAccountName() {
+        String fromAccountName = getAccountName(toAccountID, fromAccountID);
+        String toAccountName = getAccountName(toAccountID, fromAccountID);
 
-		String randomElement = names.get(random.nextInt(names.size()));
-		return randomElement;
-	}
+        fullTransactions.add(new FullTransaction(fromAccountID, toAccountID, amount, fromAccountName, toAccountName));
+    }
+
+    private String getAccountName(int toAccountID, int fromAccountID) {
+        if (uniqueNames.containsKey(toAccountID))
+            return uniqueNames.get(toAccountID);
+
+        else if (uniqueNames.containsKey(fromAccountID))
+            return uniqueNames.get(fromAccountID);
+
+        else {
+            String randomElement = names.get(random.nextInt(names.size()));
+            uniqueNames.put(fromAccountID, randomElement);
+            return randomElement;
+        }
+
+    }
 
 
 }
